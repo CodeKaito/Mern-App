902bfa965dc4876f4faaada9640c65eb539d4d34
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-03-29 13:34:52 +0200

    update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,22 @@
+import "dotenv/config";
+import mongoose from "mongoose";
 import express from "express";
 
 const app = express();
-const port = 5000;
 
 app.get("/", (req, res) => {
     res.send("Hello, world!");
 });
 
-app.listen(port, () => {
-    console.log("Server running on port: " + port)
-});
+const port = process.env.PORT;
+
+mongoose.connect(process.env.MONGO_CONNECTION_STRING!)
+    .then(() => {
+        console.log("DB connected");
+
+        app.listen(port, () => {
+            console.log("Server running on port: " + port)
+        });
+    })
+    .catch(console.error);
+
